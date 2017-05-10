fb9837778bea18cd7fea02606f0c99e121911b61
ryadav <ryadav@mummacl01s-MacBook-Pro.local>
2017-05-10 14:10:57 +0530

    changes in user service

--- controller/userController.js
+++ controller/userController.js
@@ -12,6 +12,21 @@ module.exports = {
      * @param {Object} response body
      */
     addUser: function(req, res) {
+        var userObject;
+
+        if(req.body.email){
+            userObject={
+                email:req.body.email,
+                password:req.body.password
+            }
+            userService.addUser(userObject)
+                .then(function (userData) {
+                    res.send(userData).status(200).end();
+                },function (error) {
+                    res.send(error).status(403).end();
+                })
+        }
+
         //If user account cerated res.status(200).send(data)
         //else  res.status(403).send(error);
 
@@ -28,13 +43,20 @@ module.exports = {
             .then(function(userData){
                 userService.validatePassword(userData,req.body.password)
                     .then(function () {
-                        tokenService.tokenGenerate(userData)
+                        var token=tokenService.tokenGenerate(userData)
+                        res.send(token).status(200).end();
                         //send jwt token created using user data
                     },function (error) {
+                        res.send(error).status(403).end();
                         //send error message back
                     })
 
             },function(error){
+                var errObject={
+                    error:error,
+                    message:'Unable to find user'
+                }
+                res.send(errObject).status(403).end()
                 //send unable to find user
             })
 
--- services/userService.js
+++ services/userService.js
@@ -1,6 +1,8 @@
 /**
  * Created by ryadav on 4/30/17.
  */
+
+var crypto = require("crypto");
 var Q = require('q');
 module.exports={
     findUser:function (email) {
@@ -18,7 +20,55 @@ module.exports={
         //compare userdata.password with hash of password
         //compare hash in a loop so that it returns at same time interval in each case;
 
+        // ajax request to database here to fetch the user datails
+
+        setTimeout(function () {
+            if(this.encryptPassword(password)===this.encryptPassword(password)) {//compare the password provided by user with hash key stored in database
+
+                deferred.resolve(userdata)//return user data with token
+            } else {
+                deferred.reject(new Error("Invalid username or password"))
+            }
+
+        },1000)
+
         return deferred.promise;
+    },
+
+    //this is a stub return the userdata it gets with flag useer added or unable to add user
+    addUser:function (userObject) {
+        var addedUser, deferred;
+        deferred = Q.defer();
+
+        // ajax request to database here to add data to user simulating now with set timeout
+
+        setTimeout(function () {
+            if(userObject){
+                addedUser={
+                    email:userObject.email,
+                    password:this.encryptPassword(userObject.password),
+                    isAdded:true
+                }
+                deferred.resolve(addedUser);
+            } else {
+                deferred.reject(new Error('Unable to add user'))
+            }
+        },1000)
+
+        return deferred.promise;
+
+    },
+
+    /**
+     * Converts raw password string into encrypted password using crypto library.
+     *
+     * @param {String} Raw password string.
+     * @return {String} Encrypted password string.
+     */
+    encryptPassword: function(password) {
+        var salt = 'salt';
+        var passwordHash = crypto.createHash('sha256').update(salt.concat(crypto.createHash('sha256').update(password).digest('hex'))).digest('hex');
+        return passwordHash;
     }
 
 }
